5a1c7bbddd15a7203b74f22354d479a6bcf933da
BillyBob554 <110690423+BillyBob554@users.noreply.github.com>
2023-12-06 10:00:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pygame
+#Variables
+Res = (300, 300)
+running = True
 
 
 pygame.init()
+
+screen = pygame.display.set_mode((Res))
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      running = False
